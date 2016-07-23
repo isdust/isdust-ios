c6e0cff5fc943fafda0eda0c1a2d8b4fbfa031de
wzq <qq960826@gmail.com>
2016-07-23 15:31:03 +0800

    Initial Commit

---
+++ isdust_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  isdust_ios
+//
+//  Created by wzq on 7/23/16.
+//  Copyright © 2016 isdust. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
